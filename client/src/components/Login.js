7506a069509152a8c9907df3d3d6cb85a905259f
Saket Suman <sumansaket7@gmail.com>
2021-04-16 20:12:11 +0530

    Login Funcationality is done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
 
 const Login = () => {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch("/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    const data = res.json();
+
+    if (res.status === 400 || !data) {
+      window.alert("invalid details");
+      console.log("Invalid  details");
+    } else {
+      window.alert("login succesful");
+      console.log("login succesful");
+
+      history.push("/");
+    }
+  };
+
   return (
     <>
       <h1>Sign In</h1>
-      <form>
+      <form method="POST">
         <label htmlFor="email"></label>
         <input
           type="text"
           name="email"
           id="email"
           autoComplete="off"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="Your Email"
         />
         <label htmlFor="password"></label>
@@ -19,6 +52,8 @@ const Login = () => {
           name="password"
           id="password"
           autoComplete="off"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="Your Password"
         />
         <div className="form-group form-button">
@@ -27,7 +62,8 @@ const Login = () => {
             name="login"
             id="login"
             className="form-submit"
-            value="Sign in"
+            value="Log In"
+            onClick={loginUser}
           />
         </div>
       </form>
